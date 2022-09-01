1936e04af08f200273267d51f742dd42a50c7552
gf2473sm@go.minnstate.edu <gf2473sm@go.minnstate.edu>
2022-09-01 12:49:26 -0500

    Asking some bruthas for their name and age

---
+++ Test.py
@@ -0,0 +1,17 @@
+dog_year = 7
+# asking user for age input into int
+human_age = int(input("how old are you"))
+
+human_in_dog_years = human_age*dog_year
+
+print("An age ",human_age," human should be ",dog_year * human_age," in dog years")
+
+
+#
+name = input("What is you name?")
+lastname = input(" whta is your last name") 
+age = input( "how old are you")
+
+print("hi ",name,lastname,", you're ",age," years old, damn.")
+
+
