e852fb3f1b75200ef3d4556edb6b158d05a827b5
gf2473sm@go.minnstate.edu <gf2473sm@go.minnstate.edu>
2022-09-21 14:29:41 -0500

    did exam

---
+++ Class.py
@@ -0,0 +1,26 @@
+# age = int(input("How old are you"))
+
+
+# for i in range(age,age+21):
+#     print(i)
+
+number = 1
+
+for num in range (1,7):
+
+    for j in range (1,num):
+        print(j, end=" ")
+
+    print(" ")
+    
+
+   
+
+    
+
+
+
+
+
+
+
